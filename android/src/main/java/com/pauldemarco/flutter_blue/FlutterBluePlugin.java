762326359644440eb7e90c6f97238f8435ed578b
qdlmcfresh <qdlmcfresh@gmail.com>
2021-12-23 14:22:23 +0100

    Update FlutterBluePlugin.java

--- android/src/main/java/com/pauldemarco/flutter_blue/FlutterBluePlugin.java
+++ android/src/main/java/com/pauldemarco/flutter_blue/FlutterBluePlugin.java
@@ -305,7 +305,7 @@ public class FlutterBluePlugin implements FlutterPlugin, ActivityAware, MethodCa
 
                 // New request, connect and add gattServer to Map
                 BluetoothGatt gattServer;
-                gattServer = device.connectGatt(context, options.getAndroidAutoConnect(), mGattCallback, BluetoothDevice.TRANSPORT_LE);
+                gattServer = device.connectGatt(context, false, mGattCallback, BluetoothDevice.TRANSPORT_LE);
                 mDevices.put(deviceId, new BluetoothDeviceCache(gattServer));
                 result.success(null);
                 break;
